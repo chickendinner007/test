56171ec1d04deeafc1cb654aac8a5d9f868a04bf
chickendinner007 <jinkela1024@163.com>
2017-09-21 22:14:58 +0800

    你想干嘛啊？

---
+++ src/app.js
@@ -0,0 +1,7 @@
+require("./styles/app.scss")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import RootComponent from './scripts/components/RootComponent'
+
+ReactDOM.render(<RootComponent/>,document.getElementById("root"));
---
+++ src/scripts/components/RootComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class RootComponent extends React.Component {
+	render(){
+		return (
+			<div>hello world</div>
+		)
+	}
+}
+export default RootComponent
